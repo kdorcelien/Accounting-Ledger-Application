f00e535f13e1761ac6e6d44fc54b8b063551df92
kdorcelien <kdorcelien.kd@gmail.com>
2025-10-10 11:13:23 -0400

    Initial commit

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -1,31 +1,31 @@
 package com.pluralsight;
 
-public class Application {
-private String date, time, description, vendor;
-private double amount;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
-    public Application(String date, String time, String description, String vendor, double amount) {
-        this.date = date;
-        this.time = time;
+public class Application {
+    private LocalDate date;
+    private LocalTime time;
+    private String description;
+    private String vendor;
+    private double amount;
+
+
+    public Application(String description, String vendor, double amount) {
+        this.date = LocalDate.now();
+        this.time = LocalTime.now();
         this.description = description;
         this.vendor = vendor;
         this.amount = amount;
     }
 
-    public String getDate() {
+    public LocalDate getDate() {
         return date;
     }
 
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getTime() {
+    public LocalTime getTime() {
         return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
     }
 
     public String getDescription() {
@@ -54,11 +54,14 @@ private double amount;
 
     @Override
     public String toString() {
-        return "Application: " +
-                "date='" + date + '\'' +
-                ", time='" + time + '\'' +
-                ", description='" + description + '\'' +
-                ", vendor='" + vendor + '\'' +
-                ", amount=" + amount;
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("MM-dd-yyyy");
+        DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+        return "Application:\n" +
+                "Date: " + date.format(dateFormat) + "\n" +
+                "Time: " + time.format(timeFormat) + "\n" +
+                "Description: " + description + "\n" +
+                "Vendor: " + vendor + "\n" +
+                "Amount: $" + String.format("%.2f", amount);
     }
 }
--- src/main/java/com/pluralsight/Ledger.java
+++ src/main/java/com/pluralsight/Ledger.java
@@ -1,5 +1,8 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Ledger {
     public static void main(String[] args) {
         System.out.println("== Welcome to your Ledger Application ==");
@@ -7,8 +10,53 @@ public class Ledger {
     }
     public static void run() {
         while (true) {
-          //  menu();
-          /  menuSelector();
+            menu();
+          menuSelector();
         }
     }
+    public static void menu() {
+        System.out.println("What do you want to do?\n " +
+                "  1-  Add Deposit \n" +
+                "  2-  Make Payment (Debit) \n" +
+                "  3- display the ledger screen\n" +
+                "  4-  Quit the application\n");
+
+    }
+    public static void menuSelector() {
+        // ArrayList<Product> inventory = getInventory();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int option = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (option) {
+            case 1:
+                System.out.println();
+                //addDeposit();
+                break;
+            case 2:
+                System.out.print("Debit only");
+                //makePayment();
+                break;
+            case 3:
+                //ledgerScreen();
+                break;
+            case 4:
+                scanner.close();
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Incorrect option entered, try again");
+        }
+
+        System.out.println("\nPress ENTER to continue...\n");
+        scanner.nextLine();
+
+    }
+   //
+
+
+
 }
